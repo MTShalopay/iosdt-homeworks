45a232ef2b9e9ab6297407af3bf5e6b53a19215e
Shalopay <shalopay@bk.ru>
2022-12-29 22:47:54 +0300

    Решение: Домашнее задание к занятию "Работа с уведомлениями" Задача 2

--- Navigation/Notification/LocalNotificationsService.swift
+++ Navigation/Notification/LocalNotificationsService.swift
@@ -8,26 +8,39 @@
 import Foundation
 import UIKit
 import UserNotifications
-class LocalNotificationsService {
+class LocalNotificationsService: UIViewController {
     let center = UNUserNotificationCenter.current()
     
     func registeForLatestUpdatesIfPossible() {
+        registerUpdatesCategory()
         center.requestAuthorization(options: [.sound, .badge, .provisional]) { (success, error) in
             if error != nil {
                 print("ERROR: \(String(describing: error))")
             }
             if success {
                 self.notificationTimeInterval(hour: 19, minute: 00, repeats: true)
-            
             }
         }
     }
     
+    private func registerUpdatesCategory() {
+        let actionOk = UNNotificationAction(identifier: "actionOk", title: "Пойду", options: .destructive)
+        let actionCancel = UNNotificationAction(identifier: "actionCancel", title: "Не пойду", options: .authenticationRequired)
+        let testCategory = UNNotificationCategory(identifier: "updates",
+                                                  actions: [actionOk, actionCancel],
+                                                  intentIdentifiers: [],
+                                                  options: .customDismissAction)
+        center.setNotificationCategories([testCategory])
+    }
+    
+    
     private func notificationTimeInterval(hour: Int, minute: Int, repeats: Bool) {
         let content = UNMutableNotificationContent()
+        center.delegate = self
         DispatchQueue.main.async {
             content.badge = NSNumber(value: UIApplication.shared.applicationIconBadgeNumber + 1)
         }
+        content.categoryIdentifier = "updates"
         content.sound = .default
         content.title = "Тук тук"
         content.body = "Посмотрите последние обновления"
@@ -37,5 +50,16 @@ class LocalNotificationsService {
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateMatching, repeats: repeats)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         self.center.add(request)
+    }
+}
+
+extension LocalNotificationsService: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+            case "actionOk": print("Taping actionOK")
+            case "actionCancel": print("Tapping actionCancel")
+        default:
+            print("default")
+        }
     }
 }
